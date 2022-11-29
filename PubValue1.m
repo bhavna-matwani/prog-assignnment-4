eec93f245b51e8c58ed202f9875eb3cd65073a38
Bhavna Matwani <53350565+bhavna-matwani@users.noreply.github.com>
2022-11-29 15:32:57 -0500

    Update PubValue1.m

--- PubValue1.m
+++ PubValue1.m
@@ -1,11 +1,11 @@
 function [Bool,Net] = PubValue1(ValuesSuc,ValueFail,Fee,ProbSuc,ProbPosSuc,ProbPosFail,NPos,NNeg)
 ProbSucPosNeg = ProbSuccess(ProbSuc,ProbPosSuc,ProbPosFail,NPos,NNeg);
 Net = ProbSucPosNeg*(ValuesSuc) - ((1-ProbSucPosNeg)*(ValueFail)) - (Fee*(NPos+NNeg));
-if(Net>-Fee)
+if(Net>-Fee*(NPos+NNeg))
     Bool = true;
 
 else
     Bool = false;
-    Net = -Fee;
+    Net = -Fee*(NPos+NNeg);
 end
 end
